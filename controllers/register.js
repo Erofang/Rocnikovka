9f4da390f418689c51a3cd6b8a7053f99ceefb3c
Dedline <matous.kader@seznam.cz>
2021-12-18 18:56:43 +0100

    register working

--- controllers/register.js
+++ controllers/register.js
@@ -1,24 +1,7 @@
 const db = require('../database')
 const bcrypt = require('bcrypt')
 
-<<<<<<< Updated upstream
-        exports.register = (req, res) => {
-   const {jmeno, prijmeni, mobile, email, password} = req.body
 
-
-    sql = `INSERT INTO zakaznici(jmeno, prijmeni, tel_cislo, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${mobile}','${email}','${password}')`
-    db.query(sql, (err, result) => {
-        if (err) {
-            throw err;
-        }else {
-            console.log(result)
-            res.redirect('/')
-        }
-    })
-}
-
-
-=======
 exports.register = (req, res) => {
     const {jmeno, prijmeni, mobil, email, password} = req.body
     console.log(req.body)
@@ -34,5 +17,3 @@ exports.register = (req, res) => {
 }
         
     
-
->>>>>>> Stashed changes
