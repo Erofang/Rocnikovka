d4b94a76a7687994a9d20e538906619729fa7a99
Dedline <matous.kader@seznam.cz>
2021-12-21 17:49:24 +0100

    small update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const registo = require('../controllers/register')
+const loginos = require('../controllers/login')
 
 const router = express.Router()
 
 //routa na register
 router.post('/register', registo.register)
+router.post('/login', loginos.login)
 
 
 
