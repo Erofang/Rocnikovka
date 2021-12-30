2fb3ca913f87cdf053394ecfc4b681d19bd3f68f
Dedline <matous.kader@seznam.cz>
2021-12-30 17:36:23 +0100

    login
    opravit login
    přídáno šifrování hesla

--- controllers/login.js
+++ controllers/login.js
@@ -3,10 +3,28 @@ const bcrypt = require('bcrypt');
 
 
 exports.login = (req, res) => {
-    const email = req.body.email
-    const password = req.body.password
+    const {email, password} = req.body;
 
     db.query(
-        
-    )
+		// Hledání stejného emailu v databázi
+		'SELECT email FROM zakaznici WHERE email = ?',
+		[email],
+		async (err, results) => {
+			if (err) throw err;
+			// Pokud se email nenašel, napíše se zpráva dole
+			if (results.length == 0) {
+				return res.redirect('/login');
+			} else {
+				const hashedPassword = results[0].password;
+				// get the hashedPassword from result
+				// Porovnávání hesla s hasnutym heslem
+				if (await bcrypt.compare(password, hashedPassword)) {
+					return res.render('/');
+					// Když se nenajde, pošle se "password incorrect"
+				} else {
+					return res.render('/login')
+				}
+			}
+		}
+	);
 }
--- controllers/register.js
+++ controllers/register.js
@@ -6,8 +6,12 @@ const bcrypt = require('bcrypt')
 exports.register = (req, res) => {
     const {jmeno, prijmeni, mobil, email, password} = req.body
     console.log(req.body)
+
+    //zašifrování hesla
+    const hashedPassword =  bcrypt.hashSync(password, 10);
+    console.log(hashedPassword);
         //vložíme hodnoty z formuláře do databaze
-        db.query(`INSERT INTO zakaznici(jmeno, prijmeni, mobil, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${mobil}','${email}','${password}')`, 
+        db.query(`INSERT INTO zakaznici(jmeno, prijmeni, mobil, email, heslo) VALUES ('${jmeno}','${prijmeni}', '${mobil}','${email}','${hashedPassword}')`, 
         (err, result) => {
             if(err) {
                 console.log(err);
