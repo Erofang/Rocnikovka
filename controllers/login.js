8af14711ef9a84b88f05c5a3ab46ed2fc451a551
Dedline <matous.kader@seznam.cz>
2022-01-05 21:54:40 +0100

    login update
    still not working

--- controllers/login.js
+++ controllers/login.js
@@ -2,29 +2,19 @@ const db = require('../database');
 const bcrypt = require('bcrypt');
 
 
-exports.login = (req, res) => {
-    const {email, password} = req.body;
 
-    db.query(
-		// Hledání stejného emailu v databázi
-		'SELECT email FROM zakaznici WHERE email = ?',
-		[email],
-		async (err, results) => {
-			if (err) throw err;
-			// Pokud se email nenašel, napíše se zpráva dole
-			if (results.length == 0) {
-				return res.redirect('/login');
-			} else {
-				const hashedPassword = results[0].password;
-				// get the hashedPassword from result
-				// Porovnávání hesla s hasnutym heslem
-				if (await bcrypt.compare(password, hashedPassword)) {
-					return res.render('/');
-					// Když se nenajde, pošle se "password incorrect"
-				} else {
-					return res.render('/login')
-				}
-			}
+
+exports.login = (req, res) => {
+	const {email, password} = req.body;
+	console.log(req.body);
+
+	db.query(`SELECT * FROM zakaznici WHERE email = '${email}'`),
+	(err, email, password, result) => {
+		if(err) {
+			console.log(err);
+		}else if(email == result[i].email && bcrypt.verify(password, result[i].heslo)){
+			console.log(password);
+			return res.redirect('/');
 		}
-	);
+	}
 }
