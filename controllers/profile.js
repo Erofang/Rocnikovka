53ce8c1c72e9098380f1f67fdecedf9e0377a9dd
Dedline <matous.kader@seznam.cz>
2022-06-01 09:35:17 +0200

    prifleedit update

--- controllers/profile.js
+++ controllers/profile.js
@@ -10,9 +10,19 @@ router.get('/', (req, res) => {
         name: req.user.jmeno,
         surname: req.user.prijmeni,
         tel: req.user.mobil,
-        email: req.user.email, 
+        email: req.user.email,
+        heslo: req.user.heslo,
+        id: req.user.id_zak
     })
-})
+    console.log(req.user.id_zak);
+});
+
+router.post('/', async (req, res) => {
+    const { jmeno, prijmeni, mobil, email, heslo, id } = req.body;
+	console.log(req.body);
+    await Profile.editProfile( jmeno, prijmeni, mobil, email, heslo, id );
+	res.redirect('/')
+});
 
 
 
--- models/Profile.js
+++ models/Profile.js
@@ -1,11 +1,15 @@
 const db = require('../database');
 
-exports.editProfile = () => {
-    return new Promise((resolve, reject) => {
-        try {
-            let sql = `UPDATE zakaznici SET jmeno='${jmeno}',prijmeni='${prijmeni}',mobil='${telefon}',email='${email}',heslo='${hashedPassword}' WHERE id_zak = ?'`;
-        } catch (err) {
 
-        }
-    })
-}
+exports.editProfile = (jmeno, prijmeni, mobil, email, heslo, ID) => {
+	return new Promise((resolve, reject) => {
+		try {  let sql = `UPDATE zakaznici SET jmeno = '${jmeno}', prijmeni = '${prijmeni}', mobil = '${mobil}', email = '${email}', heslo = '${heslo}' WHERE id_zak = '${ID}'`;
+		db.query(sql, (err) => {
+			if (err) throw err;
+			resolve(true);
+		});
+	} catch (err) {
+		reject(err);
+		}
+	});
+};
